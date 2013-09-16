aa0efe7ea688c4b40539e38645e23651d43dece7
raydipasupil@gmail.com <raydipasupil@gmail.com>
2013-09-16 11:50:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brada::Application.config.secret_key_base = '0a08bf61a875a60a3be436cb79fe691b1e1de27f7b35db93b1702280e0f4b1b858940ef15077d2c3f423e441e22308d8d5e9b5417b275ab140abe16737eb982e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Brada::Application.config.session_store :cookie_store, key: '_brada_session'
