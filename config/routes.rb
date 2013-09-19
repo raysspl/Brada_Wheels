809cf4c7f7fc8f7567d8a7e8f10b5a80425f4cc6
raydipasupil@gmail.com <raydipasupil@gmail.com>
2013-09-18 20:37:03 -0700

    Trying new designs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Brada::Application.routes.draw do
   get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
